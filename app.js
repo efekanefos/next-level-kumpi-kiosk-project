7c04844741cb2105e063c8ea5fd73a9a393a952c
Efekan EFE <efekanefos@gmail.com>
2022-02-25 15:12:08 +0300

    dealsDone

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const cards = document.querySelectorAll(".sweetProducts");
 const popUpCard = document.querySelector(".popUpCard");
 const popUpCloseBtn = document.querySelector(".popUpCard__closeBtn");
-popUpCard.style.display = "none";
-cards.forEach((card) => {
-  card.addEventListener("click", (e) => {
-    popUpCard.style.display = "flex";
+const logoSection__textSpan = document.querySelector(".logoSection__textSpan");
+if (logoSection__textSpan.textContent === "/ SWEET POTATOES") {
+  popUpCard.style.display = "none";
+  cards.forEach((card) => {
+    card.addEventListener("click", (e) => {
+      popUpCard.style.display = "flex";
+    });
+  });
+  popUpCloseBtn.addEventListener("click", () => {
+    popUpCard.style.display = "none";
+  });
+} else {
+}
+
+const cancelOrderPopup = document.querySelector(".cancelOrderPopup");
+const productCounter__minusBtns = document.querySelectorAll(".productCounter__minusBtn");
+const buttons__no = document.querySelector(".buttons__no");
+cancelOrderPopup.style.display = "none";
+productCounter__minusBtns.forEach((minusBtn) => {
+  minusBtn.addEventListener("click", () => {
+    cancelOrderPopup.style.display = "flex";
   });
 });
-popUpCloseBtn.addEventListener("click", () => {
-  popUpCard.style.display = "none";
+buttons__no.addEventListener("click", () => {
+  cancelOrderPopup.style.display = "none";
 });
